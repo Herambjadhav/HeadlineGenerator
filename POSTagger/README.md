9e25a7029c09d1e82e2e532dadb2c23acca7b383
Gareth Dsouza <gdsouza@usc.edu>
2016-11-13 00:57:19 -0800

    Implemented POS tagging and Chunking of NP

--- POSTagger/README.md
+++ POSTagger/README.md
@@ -5,4 +5,32 @@ Takes the input summarized text file and uses nltk package to identify Parts of 
 Run:
 python taggerHG.py sum_test1.txt 
 
-where sum_tese1.txt is the input summarized file. 
+where sum_tese1.txt is the input summarized file.
+
+
+References:
+<ul>
+
+	
+	<li>Summarization test tool
+		<a href="http://autosummarizer.com/index.php">http://autosummarizer.com/index.php</a>
+	</li>
+	
+	<li>Part of speech test tool
+		<a href="http://parts-of-speech.info/">http://parts-of-speech.info/</a>
+	</li>
+
+	<li>Part of speech labels
+		<a href="https://www.ling.upenn.edu/courses/Fall_2003/ling001/penn_treebank_pos.html">https://www.ling.upenn.edu/courses/Fall_2003/ling001/penn_treebank_pos.html</a>
+	</li>
+
+	<li>Chunking
+		<a href="https://www.eecis.udel.edu/~trnka/CISC889-11S/lectures/dongqing-chunking.pdf">https://www.eecis.udel.edu/~trnka/CISC889-11S/lectures/dongqing-chunking.pdf</a>
+	</li>
+
+	<li>POS and Chunking using NLTK
+		<a href="http://www.nltk.org/book/ch07.html">http://www.nltk.org/book/ch07.html</a>
+	</li>
+
+	
+</ul>
--- POSTagger/taggerHG.py
+++ POSTagger/taggerHG.py
@@ -4,26 +4,53 @@ import sys
 
 def readFile(filePath):
     with open(filePath, 'r', encoding="latin1") as f:
+        # Open file and read Lines
         fileLines = f.read().splitlines()
-        # text = word_tokenize("And now for something completely different")
+
+        # Check if text file is not empty
         if(len(fileLines) > 0):
-            summarizationText = fileLines[0]
-            return summarizationText
+            # TO DO - confirm how many lines of the summarized text are to be considered?
+            summarizedText = fileLines[0]
+            return summarizedText
         else:
             return None
 
 
-def initTagger(filePath):
-    if
-    readFile(filePath)
+def extractNounPhrasesFromTree(tokenTree):
+    g= "S"
 
+def initTagger(filePath):
+    if(filePath):
+        summarizedText = readFile(filePath)
+        if(not summarizedText is None):
+            # split the input text into tokens
+            tokens = nltk.word_tokenize(summarizedText,language='english')
+
+            # perform POS Tagging on the tokens
+            posTokens = nltk.pos_tag(tokens)
+
+            # pattern to recognize noun phrases
+            pattern = "NP: {<DT>?<JJ>*<NN>*<NNP>*<NNS>*}"
+            # pattern = "NP: { < DT | PP\$ > ? < JJ > * < NN >}{ < NNP > +}{ < NN > +}"
+
+            # create chunk parser
+            NPChunker = nltk.RegexpParser(pattern)
+
+            # parse the sentences of tokens
+            result = NPChunker.parse(posTokens)
+
+            extractNounPhrasesFromTree(result)
+            g = "S"
+        else:
+            return "No text in input file"
 
 if __name__ == "__main__":
     # Get the file path from command line
     # requires sys package
     # Sanity check for input filename
     if(len(sys.argv)>1):
-        filePath = sys.argv[1]
+        # filePath = sys.argv[1]
+        filePath = "sum_test3.txt"
     else:
         print("Error file not found. Please check that the file exists\n")
         print("File path to be specified as input command line argument\n")
