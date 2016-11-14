e7cfa17d1e5be880f3da473deb1c1e2a45177007
Gareth Dsouza <gdsouza@usc.edu>
2016-11-14 04:58:37 -0800

    program to extract article text from HTML #To Fix UNICODE ERROR

--- POSTagger/taggerHG.py
+++ POSTagger/taggerHG.py
@@ -2,9 +2,6 @@ import nltk
 import sys
 
 
-grammar1 = nltk.CFG.fromstring("""
-    S -> NP VP
-""")
 
 def readFile(filePath):
     with open(filePath, 'r', encoding="latin1") as f:
@@ -20,8 +17,54 @@ def readFile(filePath):
             return None
 
 
-def extractNounPhrasesFromTree(tokenTree,posTokens,tokens):
-    g= "S"
+def getNNPNode(nodeList):
+    NPNodeList = list()
+
+    for eachnode in nodeList:
+        if(hasattr(eachnode,"_label")):
+            if(eachnode._label == "NP"):
+                for nodes in eachnode:
+                    if(nodes[1] == 'NNP' or nodes[1] == 'NNPS'):
+                        NPNodeList.append(eachnode)
+
+    if(NPNodeList):
+        return (NPNodeList)
+    else:
+        return None
+
+
+def makePossibleHeadlines(result):
+
+    summarizedTreeNodes = []
+
+    # Remove preposed adjuncts based on commas
+    treeLength = len(result)
+    for nodeIndex in range(0,treeLength):
+        # Check if its not a sub tree
+        if(not hasattr(result[nodeIndex],"_label")):
+            if(result[nodeIndex][0] == ','):
+                nodeList = [result[nodeIndex - 1],result[nodeIndex + 1]]
+                NNPNodes = getNNPNode(nodeList)
+                if(NNPNodes):
+                    summarizedTreeNodes += NNPNodes
+
+
+
+    print(summarizedTreeNodes)
+
+
+
+
+def getDescribersCommas(tokens):
+    commaList= list()
+    commaIndex = 0
+    for eachtoken in tokens:
+        if("," in eachtoken):
+            # Check if the comma is used to describe
+            # and not used as a punctuation to list a set of things
+            # eg red, green and blue
+
+            commaIndex += 1
 
 def initTagger(filePath):
     if(filePath):
@@ -46,7 +89,7 @@ def initTagger(filePath):
             # create chunk parser
             # NPChunker = nltk.RegexpParser(patternNP)
             NPChunker = nltk.RegexpParser('''
-                            NP: {<DT>? <JJ>* <NN>* <NNP>* <NNS>*} # NP modified
+                            NP: {<DT>? <JJ>* <NN>* <NNP>* <NNS>* <PP>*} # NP modified
                             P: {<IN>}           # Preposition
                             V: {<V.*>}          # Verb
                             PP: {<P> <NP>}      # PP -> P NP
@@ -59,7 +102,7 @@ def initTagger(filePath):
             # resultVP = VPChunker.parse(posTokens)
 
 
-            g = "S"
+            makePossibleHeadlines(resultNP)
         else:
             return "No text in input file"
 
@@ -67,14 +110,23 @@ if __name__ == "__main__":
     # Get the file path from command line
     # requires sys package
     # Sanity check for input filename
-    if(len(sys.argv)>1):
-        # filePath = sys.argv[1]
-        filePath = "sum_test1.txt"
-    else:
-        print("Error file not found. Please check that the file exists\n")
-        print("File path to be specified as input command line argument\n")
-        exit(0)
+    filePath = "Data/sum_test3.txt"
 
+    # if(len(sys.argv)>1):
+    #     filePath = sys.argv[1]
+    #     filePath = "Data/sum_test1.txt"
+    # else:
+    #     print("Error file not found. Please check that the file exists\n")
+    #     print("File path to be specified as input command line argument\n")
+    #     exit(0)
+
+    filePath = "Data/sum_test1.txt"
+    initTagger(filePath)
+
+    filePath = "Data/sum_test2.txt"
+    initTagger(filePath)
+
+    filePath = "Data/sum_test3.txt"
     initTagger(filePath)
 
 
