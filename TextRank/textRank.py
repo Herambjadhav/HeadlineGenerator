d8c0267482853e1bb14ac689b53ba4b8a63dfc07
Praneet Kalluri <praneet97@gmail.com>
2016-11-15 02:07:49 -0800

    Tokenized content

--- TextRank/textRank.py
+++ TextRank/textRank.py
@@ -1,1 +1,27 @@
 # Python file for Text Rank
+import sys
+import nltk
+
+
+def keyphrases(content):
+    texttokens = nltk.word_tokenize(content)
+    taggedtokens = nltk.pos_tag(texttokens)
+    filteredtokens = [x for x in taggedtokens if x[1] in ['NN', 'JJ', 'NNP']]
+    dotlesstokens = [(x[0].replace('.', ''), x[1]) for x in filteredtokens]
+    print(dotlesstokens)
+
+
+def sentences(content):
+    print("Nothing to see here, move on! :P")
+
+
+def main():
+    filename = sys.argv[1]
+    filecontent = open(filename, "r")
+    content = filecontent.read()
+    # print(content)
+    keyphrases(content)
+    sentences(content)
+
+if __name__ == '__main__':
+    main()
